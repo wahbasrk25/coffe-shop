99452d3c12675559405af6caf9c83a90f75ce4be
wahbasrk25 <wahbasrk25@gmail.com>
2023-05-01 22:31:06 +0300

    coffe shop

---
+++ java script/script.js
@@ -0,0 +1,252 @@
+
+
+
+function coffe(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *10;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     prompt("enter tour value..........");
+ }
+
+}
+
+
+function tea(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *5;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+    alert("enter tour value..........");
+ }
+
+} 
+
+
+function fenugreek(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *5;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+
+function yansoon(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *5;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function Mint(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *5;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+
+function Nescafe(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *10;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function black_nescafe(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *10;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function cafe_oly(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *15;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function latti(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *15;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+
+function light(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *15;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function mocka(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *15;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function milk_cheek(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *15;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function banana(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *10;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function stropery(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *10;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+
+function mango(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *10;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function guffa(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *10;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function coctel(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *15;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function peach(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *10;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+
+function tot(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *10;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+function appel(){
+                
+    var price1 =parseInt(document.getElementById("getprise").value);
+    var x = price1 *10;
+    var setresult= document.getElementById("show").innerHTML=x; 
+    
+ if(price1==0){
+     alert("enter tour value..........");
+ }
+
+}
+
+
+
